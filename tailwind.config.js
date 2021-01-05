08ed613aa2fbf98cb7aad1e8f86333a4bd13abed
Santiago Castano <santiago.castanom@gmail.com>
2021-01-05 09:48:52 -0600

    add commands to run

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      yellow: "#FFEA00",
+      gray: "#808080",
+      black: colors.black,
+      white: colors.white,
+    },
     extend: {},
   },
   variants: {
